e4fa50e75706332e5587ab205273ba543ca9eb82
ancaifeng <ancaifeng123@126.com>
2019-09-23 12:58:04 +0800

    commit 4

---
+++ src/com/ancf/demo/Test2.java
@@ -0,0 +1,10 @@
+package com.ancf.demo;
+
+/**
+ * Created by Administrator on 2019/9/23.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
